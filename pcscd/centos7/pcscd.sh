91caa08defa2c761f2d72bf0c1277e93fb06b643
Glen Nakamura <glen@imodulo.com>
2018-03-18 13:59:20 -1000

    pcscd: replace "${HOME}" with ~

--- pcscd/centos7/pcscd.sh
+++ pcscd/centos7/pcscd.sh
@@ -7,7 +7,7 @@ docker image inspect pcscd &>/dev/null \
 running=$(docker container inspect -f '{{.State.Running}}' pcscd 2>/dev/null) \
   || docker run -d --name pcscd -v pcscd:/var/run/docker-pcscd -p 2222:22 pcscd
 [ "${running}" = 'false' ] && docker start pcscd
-ssh-keygen -f "${HOME}"/.ssh/known_hosts -R [localhost]:2222 &>/dev/null
+ssh-keygen -f ~/.ssh/known_hosts -R [localhost]:2222 &>/dev/null
 ssh -N -n -l pcscd -i "${basedir}"/pcscd-key -p 2222 \
   -R /var/run/docker-pcscd/pcscd.comm:/tmp/pcscd.comm \
   -o StrictHostKeyChecking=no localhost &>/dev/null &
