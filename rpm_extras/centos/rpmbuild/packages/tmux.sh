ab634078aa9f9aa99c7fc4f981154af60b56c0a3
Glen Nakamura <glen@imodulo.com>
2018-05-26 20:03:25 -1000

    rpm_extras: update tmux package

--- rpm_extras/centos/rpmbuild/packages/tmux.sh
+++ rpm_extras/centos/rpmbuild/packages/tmux.sh
@@ -8,12 +8,14 @@ build_dependencies+=(
 )
 
 build_tmux () {
-  local uri=https://kojipkgs.fedoraproject.org/packages/tmux/2.6/3.fc28/src
-  local pkg=tmux-2.6-3.fc28.src.rpm
+  local uri=https://kojipkgs.fedoraproject.org/packages/tmux/2.7/1.fc28/src
+  local pkg=tmux-2.7-1.fc28.src.rpm
   local spec=~/rpmbuild/SPECS/tmux.spec
   curl -L -R -O ${uri}/${pkg}
   rpm -i ${pkg}
-  sed -i -e "s|libevent-devel|libevent${centos6:+2}-devel|" ${spec}
+  build_centos6 && sed -i \
+    -e 's|libevent-devel|libevent2-devel|' \
+    -e 's|^%make_build|#%make_build|' ${spec}
   rpmbuild -bb ${spec}
   cp ~/rpmbuild/RPMS/x86_64/tmux-2.*.rpm rpms/
 }
