e9e5305edd5bcc89d49aafb43887c4a24de889c5
Glen Nakamura <glen@imodulo.com>
2018-10-13 07:19:09 -1000

    rpm_extras: add export_functions

--- rpm_extras/centos/rpmbuild/core/export.sh
+++ rpm_extras/centos/rpmbuild/core/export.sh
@@ -4,20 +4,25 @@ export_references () {
   for _token in $(declare "$@" | sed -e 's|[^A-Za-z0-9_]| |g'); do
     [[ "${_token}" == [a-z]* ]] || continue
     declare -p ${_token} &>/dev/null \
-      && add_items_to_list _build_export_variables ${_token} \
+      && add_items_to_list _export_variables ${_token} \
       && export_references -p ${_token}
     declare -f ${_token} &>/dev/null \
-      && add_items_to_list _build_export_functions ${_token} \
+      && add_items_to_list _export_functions ${_token} \
       && export_references -f ${_token}
   done
 }
 
+export_functions () {
+  _export_variables=()
+  _export_functions=("$@")
+  export_references -f "$@"
+  echo "$(declare -p "${_export_variables[@]}")"
+  echo "$(declare -f "${_export_functions[@]}")"
+  unset _export_variables _export_functions
+}
+
 export_tasks () {
   local _tasks="$1[@]"
-  _build_export_variables=()
-  _build_export_functions=("${!_tasks}")
-  export_references -f "${!_tasks}"
-  echo "$(declare -p "${_build_export_variables[@]}")"
-  echo "$(declare -f "${_build_export_functions[@]}")"
+  export_functions "${!_tasks}"
   echo "$(declare -F "${!_tasks}")"
 }
