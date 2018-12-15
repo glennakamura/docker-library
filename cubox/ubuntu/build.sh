4a0acdb07a969d78452709e4252d0bf58144d98e
Glen Nakamura <glen@imodulo.com>
2018-12-15 09:20:44 -1000

    cubox: initial docker files

---
+++ cubox/ubuntu/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+UBOOT_VERSION=v2018.11
+KERNEL_VERSION=4.19.9
+umask 022
+mkdir -p cubox/root/boot
+cd cubox
+
+# compile u-boot
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+git clone git://git.denx.de/u-boot.git
+cd u-boot
+git checkout ${UBOOT_VERSION}
+make mx6cuboxi_defconfig
+make
+cd ..
+
+# compile kernel
+export INSTALL_PATH=$(pwd)/root/boot
+export INSTALL_MOD_PATH=$(pwd)/root
+curl -L -R -O \
+  https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL_VERSION}.tar.xz
+tar xJf linux-${KERNEL_VERSION}.tar.xz
+cd linux-${KERNEL_VERSION}
+make imx_v6_v7_defconfig
+make zImage
+make zinstall
+make modules
+make modules_install
+make dtbs
+make dtbs_install
+cd ..
