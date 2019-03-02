536dee3b92950ec50e6ceda3c898cc3f9afd436f
Glen Nakamura <glen@imodulo.com>
2019-03-02 10:19:30 -1000

    abuild: add custom apk repository

--- abuild/alpine/setup.sh
+++ abuild/alpine/setup.sh
@@ -7,8 +7,13 @@ if [ "${arch}" = x86 ] && [ -L /bin/uname ]; then
   chmod 755 /bin/uname
 fi
 
+key=glennakamura.gitlab.io-5c27b806.rsa.pub
+curl -L -R -o /etc/apk/keys/${key} \
+  https://glennakamura.gitlab.io/alpine/keys/${key}
+echo https://glennakamura.gitlab.io/alpine/testing >>/etc/apk/repositories
+
 adduser -D -s /bin/bash build
 addgroup build abuild
 echo 'build ALL=(ALL) NOPASSWD: ALL' >/etc/sudoers.d/build
 chmod 755 /home/build
-su - build -c "abuild-keygen -a -i"
+su - build -c 'abuild-keygen -a -i'
